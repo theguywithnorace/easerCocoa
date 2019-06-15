ad89521f55ece9b8eff4280788b706b4c51e0a6b
timotheein <easer.ceo@gmail.com>
2019-06-16 00:51:16 +0200

    Fbsdk ok for android

--- android/app/src/main/java/com/easer/easercocoa/MainApplication.java
+++ android/app/src/main/java/com/easer/easercocoa/MainApplication.java
@@ -1,8 +1,13 @@
 package com.easer.easercocoa;
 
 import android.support.multidex.MultiDexApplication;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -31,6 +36,12 @@ import java.util.List;
 
 public class MainApplication extends MultiDexApplication implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -41,6 +52,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+        new FBSDKPackage(mCallbackManager),
         new RNFirebasePackage(),
         // add/remove these packages as appropriate
         new RNFirebaseAdMobPackage(),
